faddee18e37eac76a482d220d9fb944a2796d333
Chris <to.chris.lindstrom@gmail.com>
2021-02-17 13:52:28 +0100

    Copied New Todo files to this project

---
+++ src/components/NewTodoForm.js
@@ -0,0 +1,30 @@
+import { useContext, useState } from 'react';
+import { TodoContext } from '../contexts/TodoContext';
+import style from '../css/NewTodo.module.css';
+
+const NewTodoForm = () => {
+  const { addTodo } = useContext(TodoContext);
+  const [todoText, setTodoText] = useState('');
+
+  const todoTextHandler = (e) => {
+    setTodoText(e.target.value);
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    addTodo(todoText);
+    setTodoText('');
+  }
+
+  return ( 
+    <form onSubmit={submitHandler} className={style.form}>
+      <input  type="text" 
+              onChange={todoTextHandler} 
+              value={todoText} 
+              name="description"/>
+      <input type="submit" value="ADD"/>
+    </form>
+   );
+}
+ 
+export default NewTodoForm;
