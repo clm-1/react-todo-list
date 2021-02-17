994b9253fd974898fe3db518e68ccd50e7b94db5
Chris <to.chris.lindstrom@gmail.com>
2021-02-17 13:18:40 +0100

    Copied base-files from old folder
    Will start to recreate app as a git-project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+// import { BrowserRouter as Router, Route } from 'react-router-dom';
+// import TodoContextProvider from './contexts/TodoContext';
+
+// import Navbar from './components/Navbar';
+// import Home from './pages/Home';
+// import About from './pages/About';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Router>
+        <TodoContextProvider>
+          <Navbar />
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </TodoContextProvider>
+      </Router> */}
+    </div>
+  );
+}
+
+export default App;
