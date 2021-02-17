1cd24e635aa303e100c6591f980fb27671ad6dfa
Chris <to.chris.lindstrom@gmail.com>
2021-02-17 13:50:09 +0100

    Added Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+// import NewTodoForm from '../components/NewTodoForm';
+// import TodoList from '../components/TodoList';
+
+const Home = () => {
+  return ( 
+    <div>
+      {/* <NewTodoForm />
+      <TodoList /> */}
+    </div>
+   );
+}
+ 
+export default Home;
