fd25e895a80defbdbded71c05262dcc7731b22c4
Chris <to.chris.lindstrom@gmail.com>
2021-02-17 13:53:42 +0100

    Connected todo-form to Home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,11 @@
-// import NewTodoForm from '../components/NewTodoForm';
+import NewTodoForm from '../components/NewTodoForm';
 // import TodoList from '../components/TodoList';
 
 const Home = () => {
   return ( 
     <div>
-      {/* <NewTodoForm />
-      <TodoList /> */}
+      <NewTodoForm />
+      {/* <TodoList /> */}
     </div>
    );
 }
